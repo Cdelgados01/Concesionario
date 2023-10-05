f9f2124b524f5f76daa5e3f628056360a51632df
cdelg <cdelg@Cesar_Delgado>
2023-10-05 15:17:42 -0500

    Concesionario

---
+++ src/View/EjemploJswing.java
@@ -0,0 +1,19 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class EjemploJswing {
+
+    public static void main(String[] args) {
+        EjemploListComboBox jswing = new EjemploListComboBox();
+        jswing.setVisible(true);
+        jswing.setDefaultCloseOperation(3);
+        jswing.setLocationRelativeTo(null);
+    }
+    
+}
